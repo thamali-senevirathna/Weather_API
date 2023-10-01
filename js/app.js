20955ae776d9a63f5647baf97a6bd412c4c5ae12
thamali-arunika <thamaliaruni@gmail.com>
2023-10-01 21:47:13 +0530

    update

--- js/app.js
+++ js/app.js
@@ -8,12 +8,17 @@ let apiKey="0a292b3696fb41b8af061802233009";
 
 let tempText=document.getElementById("temperature");
 let cityText=document.getElementById("city");
+let locationDetails=document.getElementById("locationDetails");
 
 let icon = document.getElementById("imgAreaWeather");
 let realFeel=document.getElementById("realTxt");
 let wind=document.getElementById("windTxt");
 let rain=document.getElementById("rainTxt");
 let uv=document.getElementById("uvTxt");
+
+let weatherStatus=document.getElementById("weatherStatus");
+let lat=document.getElementById("lat");
+let lon=document.getElementById("lon");
 //fetchWeatherData("Anuradhapura");
 
 fetchWeatherData("Panadura");
@@ -32,17 +37,9 @@ function fetchWeatherData(location) {
           wind.innerHTML=data.current.wind_kph+ " kph";
           rain.innerHTML=data.current.precip_mm+ " mm";
           uv.innerHTML=data.current.uv;
-        //  countryP.text(data["location"]["country"]);
-        //  idP.text (data["current"]["temp_c"] + "°C");
-        //  latP.text (data["location"]["lat"]);
-        //  lonP.text (data["location"]["lon"]);
-        //  nameP.text (data["location"]["name"]);
-        //  regionP.text(data["location"]["region"]);
-        //  urlP.text(data["current"]["condition"]["text"]);
-        //  humidity.text(data["current"]["humidity"]);
-        //  tz_id.text(data["location"]["tz_id"]);
-        //  wind_kph.text(data["current"]["wind_kph"] + "kph");
-        //  img.src = data["current"]["condition"]["icon"];
+        weatherStatus.innerHTML="Today is "+data.current.condition.text+" day ."
+        lat.innerHTML=data.location.lat+" ϕ";
+        lon.innerHTML=data.location.lon+" λ ";
       }
    });
   }
